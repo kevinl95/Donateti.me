5905b1a05c2202fdbd2ed79923c20b2a12c83b97
kevinl95 <kevin.loeffler@colorado.edu>
2022-01-31 17:49:37 -0700

    Transitioning to Express.

---
+++ server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+var express  = require('express');
+var app      = express();                               
+var morgan = require('morgan');            
+var bodyParser = require('body-parser');    
+var cors = require('cors');
+ 
+app.use(morgan('dev'));                                        
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, './build')));
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+  });
+ 
+app.use(express.static('build'));
+app.set('port', process.env.PORT || 3001);
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
